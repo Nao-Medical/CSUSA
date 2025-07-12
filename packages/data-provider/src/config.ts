03e7f852d220c9420fd3ed9e3457ecf485a0ad0f
saulatdev <zubairs@naomedical.com>
2025-07-12 22:10:40 +0500

    comit

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -636,7 +636,7 @@ export const alternateName = {
   [EModelEndpoint.assistants]: 'Assistants',
   [EModelEndpoint.agents]: 'Agents',
   [EModelEndpoint.azureAssistants]: 'Azure Assistants',
-  [EModelEndpoint.azureOpenAI]: 'CSUSA',
+  [EModelEndpoint.azureOpenAI]: 'CSUSAooo',
   [EModelEndpoint.chatGPTBrowser]: 'ChatGPT',
   [EModelEndpoint.gptPlugins]: 'Plugins',
   [EModelEndpoint.google]: 'Google',
